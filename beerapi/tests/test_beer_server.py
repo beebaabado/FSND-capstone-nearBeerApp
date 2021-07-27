c242fa53ef7237730406e18389c65982d69a3282
Connie Compos <beebaabado@gmail.com>
2021-07-27 17:24:01 -0600

    unittest files, changed heading on login page, created config.py with defaults, new base data model

--- beerapi/tests/test_beer_server.py
+++ beerapi/tests/test_beer_server.py
@@ -5,7 +5,9 @@
 # Full Stack Web Developer Nanodegree Nearbeer API Backend unittests
 # for testing capstone project API used by Udacity by custom Beer is Near app
 # 
-# must run from package root in folder backend
+# uses temporary auth0 bearer token in config.py 
+# must run from package root in folder backend  using command line:
+# python -m beerapi.tests.test_beer_server --verbose
 
 import os, sys
 import unittest
@@ -40,7 +42,7 @@ class BeerServerTestCase(unittest.TestCase):
         #print(f'BREWER TOKEN: {test_brewer}')
         test_beer_lover = self.app.config['TEST_BEER_LOVER_AUTH_TOKEN']
         #print(f'BEER LOVER TOKEN: {test_beer_lover}')
-
+        test_invalid_token = self.app.config['TEST_INVALID_TOKEN']
         # Create authorization token
         self.headers_beer_lover = [
                 ('Content-Type', 'application/json'),
@@ -52,115 +54,214 @@ class BeerServerTestCase(unittest.TestCase):
                 ('Authorization', f'Bearer {test_brewer}')
             ]   
 
-        # # default data
-        # # get last question in question table for deletion as default
-        # self.question_to_delete_id = 0
-        # self.player_to_delete_id = 0
-        # self.category_to_delete_id = 0
-        
-        # player = Player.query.order_by(Player.id.desc()).first()
-        # if player:
-        #     self.player_to_delete_id = player.id
-
-        # question = Question.query.order_by(Question.id.desc()).first()  
-        # if question:
-        #     self.question_to_delete_id = question.id
-
-        # category = Category.query.order_by(Category.id.desc()).first()
-        # if category:
-        #     self.category_to_delete_id = category.id
-
-        # self.quiz_category = {'id': 4, 
-        #                     'type':'History'}
-
-        # self.invalid_quiz_category = {'id': 100, 
-        #                     'type':'History'}
-
-
-        # self.new_player = {"name": "Trivia Olivia"}
-        # self.new_category = {"category_type": "Music"}
-
-        # self.new_question = Question (
-        #     question = "Who is widely considered to be the world's first computer programmer?",
-        #     answer = "Ada Lovelace",
-        #     difficulty = 3,
-        #     category = 4,
-        #     rating = 3
-        # )
-        # self.new_question = self.new_question.format()
-
-
-        # self.invalid_new_question = Question (
-        #     question = "Who is widely considered to be the world's first computer programmer?",
-        #     answer = "Ada Lovelace",
-        #     difficulty = 3,
-        #     category = self.quiz_category,
-        #     rating = 3
-        # )
-        # self.invalid_new_question = self.invalid_new_question.format()
-
-        # self.previous_questions = [
-        #         {
-        #         "id": 5,
-        #             "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?",
-        #             "answer": "Maya Angelou",
-        #             "category": 4,
-        #             "difficulty": 2,
-        #             "rating": 3
-        #         }, 
-        #         {
-        #             "id": 9,
-        #             "question": "What boxer's original name is Cassius Clay?",
-        #             "answer": "Muhammad Ali",
-        #             "category": 4,
-        #             "difficulty": 1,
-        #             "rating": 3
-        #         },
-        #         {
-        #             "id": 12, 
-        #             "question": "Who invented Peanut Butter?", 
-        #             "answer": "George Washington Carver", 
-        #             "category": 4, 
-        #             "difficulty": 2,
-        #             "rating": 3
-        #         },
-        #         {
-        #             "id": 23, 
-        #             "question": "Which dung beetle was worshipped by the ancient Egyptians?", 
-        #             "answer": "Scarab", 
-        #             "category": 4, 
-        #             "difficulty": 4,
-        #             "rating": 3
-        #         }]
-
+        self.headers_invalid = [
+                ('Content-Type', 'application/json'),
+                ('Authorization', f'Bearer {test_invalid_token}')
+            ]   
+        # default data
+        # get last question in question table for deletion as default
+        self.beer_to_delete_id = 0
     
+        # # Because each test case creates new class we need to set last beer id for deletion
+        # beer = Beer.query.order_by(Beer.id.desc()).first()
+        # if beer:
+        #     self.beer_to_delete_id = beer.id
+
+        self.new_beer = {
+            "abv": "12.2", 
+            "bid": "555555X", 
+            "brewery_name": "Fremont Brewing", 
+            "brewery_slug": "fremont-brewing", 
+            "last_seen": "2021-05-29 04:11:28", 
+            "major_style": "Ale", 
+            "name": "Momo Meow Brew 5000 (2021)", 
+            "rating": "2.75", 
+            "user_rating": "3.00",
+            "slug": "fremont-brewing-brew-5000-2021", 
+            "style": "Barleywine - English", 
+            "url": "https://untappd.com/b/fremont-brewingfremont-brewing-brew-5000-2021/4246950", 
+            "venue_id": "5480785"
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         #self.db.drop_all()
         pass
 
-    
     """
     Test cases run in order of function name
     """
     
-    def test_get_beers_public(self):
-        """ Test GET beers public endpoint"""
-        res = self.client().get('/beers/boulder/')
+    def test_get_beers_public_template(self):
+        """ Test GET beers public template endpoint """
+        res = self.client().get('/beers/template?city=boulder/')
         self.assertEqual(res.status_code, 200)
-        #  this is not correct function self.assertTemplateUseed('city_list.html')
+        # TODO fix test case this is not correct function self.assertTemplateUseed('city_list.html')
         #self.assertIn("beers", res.data)
         #self.assertIn("city", res.data)
 
-    def test_get_beers_(self):
-        """ Test GET beers endpoint with authentication"""
-        res = self.client().get('/beers/boulder/', headers=self.headers_brewer)
+    def test_get_beers_public_template_fail(self):
+        """ Test GET beers public template endpoint failure"""
+        res = self.client().get('/beers/template')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], "bad request")
+        self.assertEqual(data['description'], "City not specified.")
+    
+    def test_get_beers_public(self):
+        """ Test GET beers endpoint without authentication"""
+        res = self.client().get('/beers?city=erie')
         data_temp = json.loads(res.data)
-        data = data_temp[0]
+        data = data_temp
         self.assertEqual(data['success'], True)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['beers'])
         self.assertTrue(len(data['beers']))
+
+    def test_get_beers_public_fail(self):
+        """ Test GET beers endpoint without authentication bad url failure"""
+        res = self.client().get('/beers/')
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 405)
+        self.assertTrue(data['message'], "resource not found")    
+
+    def test_get_beers_with_auth(self):
+        """ Test GET beers endpoint with authentication"""
+        res = self.client().get('/beers/boulder/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['beers'])
+        self.assertTrue(len(data['beers']))
+
+    def test_get_beers_with_auth_fail(self):
+        """ Test GET beers endpoint with authentication...invalid cit failure"""
+        res = self.client().get('/beers/nocity/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 400)
+        self.assertTrue(data['message'], "bad request")
+        self.assertTrue(data['description'], "No Beers for city nocity.")
+
+    def test_get_beers_with_auth_fail(self):
+        """ Test GET beers endpoint with authentication...invalid auth token failure"""
+        res = self.client().get('/beers/nocity/', headers=self.headers_invalid)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 400)
+        self.assertTrue(data['message'], "bad request")
+
+    def test_get_styles_with_auth(self):
+        """ Test  GET styles endpoint with authentication"""
+        res = self.client().get('/styles/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['styles'])
+        self.assertEqual(len(data['styles']), data['style_count'])
+
+    def test_get_styles_with_auth_fail(self):
+        """ Test GET styles endpoint with authentication...invalid auth token failure"""
+        res = self.client().get('/styles/', headers=self.headers_invalid)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 400)
+        self.assertTrue(data['message'], "bad request")
+    
+    def test_get_beer_details_with_auth(self):
+        """ Test GET beer details for given beer id"""
+        res = self.client().get('/beer-details/', headers=self.headers_brewer, json={'beer_id':'888'})
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['beers'])
+        self.assertTrue(len(data['beers']))
+
+
+    def test_get_beer_details_with_auth_fail(self):
+        """ Test GET beer details for given invalid beer id failure"""
+        res = self.client().get('/beer-details/', headers=self.headers_brewer, json={'beer_id':'10000'})
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['error'], 422)
+        self.assertEqual(data['message'], "unprocessable")  
+
+    def test_get_beer_details_with_auth_no_data_fail(self):
+        """ Test GET beer details missing beer id failure"""
+        res = self.client().get('/beer-details/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], "bad request")  
+
+    def test_post_new_beer(self):
+        """ Test POST new beer """
+        res = self.client().post('/beers/', headers=self.headers_brewer, json=self.new_beer)
+        data = json.loads(res.data)
+        beer = Beer.query.filter(Beer.id==data['created']).one_or_none()  
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['created'], beer.id)  
+        self.beer_to_delete_id = beer.id 
+        print(f'Newly added beer: { self.beer_to_delete_id }')
+    
+    def test_post_new_beer_already_exists_fail(self):
+        """ Test POST new beer already exists failure"""
+        res = self.client().post('/beers/', headers=self.headers_brewer, json=self.new_beer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['message'], "unprocessable")  
+        self.assertEqual(data['error'], 422)
+    
+    def test_post_new_beer_no_data_fail(self):
+        """ Test POST new beer no data failure"""
+        res = self.client().post('/beers/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], "bad request") 
+
+    # NOTE:  named to force post of new beer to happen before trying to delete it
+    def test_posted_new_beer_delete_beer(self):
+        """ Test DELETE beer with beer id""" 
+        # get last beer which was just added in create test case
+        beer = Beer.query.order_by(Beer.id.desc()).first()
+        if beer:
+            self.beer_to_delete_id = beer.id
+        delete_path="/beers/" + str(self.beer_to_delete_id) + "/"
+        res = self.client().delete(delete_path, headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], self.beer_to_delete_id)  
+
+    def test_posted_new_beer_delete_beer_fail(self):
+        """ Test DELETE beer with invalid beer id - failure""" 
+        res = self.client().delete('/beers/5000/', headers=self.headers_brewer)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['message'], "resource not found")   
+        self.assertEqual(data['error'], 404)
+    
+    # def test_post_update_rating(self):
+    #     """ Test update beer rating """
+    #     res = self.client().patch('/rating/', headers=self.headers_brewer, json={ 'id':1, 'rating': '4.25'})
+    #     data = json.loads(res.data)
+    #     print (f'DATA:::::{data}')
+    #     beer = Beer.query.filter(Beer.id==data['modified']).one_or_none() 
+    #     self.assertEqual(res.status_code, 200) 
+    #     self.assertEqual(data['modified'], beer.id)
+    #     self.assertEqual(beer.user_rating, "4.25")
+    #     self.assertEqual(data['success'], True)
+        
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
