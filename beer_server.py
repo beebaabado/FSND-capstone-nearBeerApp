79ae097d7e45128a9273a635416d344d525be857
Connie Compos <beebaabado@gmail.com>
2021-08-10 22:47:28 -0600

    did some rearranging of files.

--- beer_server.py
+++ beer_server.py
@@ -6,25 +6,27 @@ from flask_cors import CORS
 from .database.models import Beer, Venue, BeerVenue, Style, db_drop_and_create_all, setup_db
 from .auth.auth import AuthError, requires_auth
 
+
 def create_app(testconfig=None):
 
     app = Flask(__name__)
 
-    #print(f'APP NAME: {__name__}')
+    # print(f'APP NAME: {__name__}')
     # debug relative imports issue print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
     app.config.from_pyfile('config.py')
-    if testconfig==True:
-        app.config['TESTING']=True
+    if testconfig:
+        app.config['TESTING'] = True
     else:
-        app.config['TESTING']=False
+        app.config['TESTING'] = False
     setup_db(app)
 
     # Set up CORS. Allow for all origins.
     CORS(app, resources={r"./*": {"origins": "*"}})
+
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
-        #response.headers.add('Access-Control-Allow-Headers', 'accept, Content-Type')
+        # response.headers.add('Access-Control-Allow-Headers', 'accept, Content-Type')
         response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS')
         return (response)
 
@@ -35,7 +37,7 @@ def create_app(testconfig=None):
     !! NOTE Make sure in folder backend/database and run command to fill database before accessing server:
     !! NOTE psql -U postgres -d nearbeer -q -f beer_with_user_rating_dump.sql -f venuetabledump.sql -f styletabledump.sql -f beervenuetabledump.sql
     '''
-    #db_drop_and_create_all()
+    # db_drop_and_create_all()
 
     # beer config file...not app config file
     filename = os.path.join(os.path.dirname(__file__), 'config_settings.json')
@@ -48,42 +50,42 @@ def create_app(testconfig=None):
     @app.route("/beers/template", methods=['GET'])
     def get_city_beer_public_template_view():
         beers_venue_list = BeerVenue.query.all()
-        beers_by_city=[]
-        
+        beers_by_city = []
+
         city = request.args.get('city')
-        if city==None:
+        if city is None:
             abort(400, "City not specified.")
 
         for beerItem in beers_venue_list:
-            venue=Venue.query.filter_by(id=beerItem.venue_id).all()
+            venue = Venue.query.filter_by(id=beerItem.venue_id).all()
             if venue[0].city.lower() == city.lower():
-                tempbeer={}
-                beer=Beer.query.filter_by(id=beerItem.beer_id).all()
+                tempbeer = {}
+                beer = Beer.query.filter_by(id=beerItem.beer_id).all()
                 # Beer class not modifiable error so convert to dict and add venue info
-                tempbeer = row2dict_lambda(beer[0])
+                tempbeer = row2dict(beer[0])
                 tempbeer['venue'] = venue[0].format()
                 beers_by_city.append(tempbeer)
 
-        return render_template('city_list.html', city=city, beers=beers_by_city)  
- 
+        return render_template('city_list.html', city=city, beers=beers_by_city)
+
     ''' returns json formatted list of beers public view for front end no authorization
     '''
     @app.route("/beers", methods=['GET'])
     def get_city_beer_public_view():
         beers_venue_list = BeerVenue.query.all()
-        beers_by_city=[]
-        
+        beers_by_city = []
+
         city = request.args.get('city')
-        if city==None:
+        if city is None:
             abort(400, "City not specified.")
 
         for beerItem in beers_venue_list:
-            venue=Venue.query.filter_by(id=beerItem.venue_id).all()
+            venue = Venue.query.filter_by(id=beerItem.venue_id).all()
             if venue[0].city.lower() == city.lower():
-                tempbeer={}
-                beer=Beer.query.filter_by(id=beerItem.beer_id).all()
+                tempbeer = {}
+                beer = Beer.query.filter_by(id=beerItem.beer_id).all()
                 # Beer class not modifiable so convert to dict and add venue info
-                tempbeer = row2dict_lambda(beer[0])
+                tempbeer = row2dict(beer[0])
                 tempbeer['venue'] = venue[0].format()
                 beers_by_city.append(tempbeer)
 
@@ -103,7 +105,7 @@ def create_app(testconfig=None):
     '''
     @app.route("/", methods=['GET'])
     @requires_auth('view:simple')
-    def index(payload): 
+    def index(payload):
         beers = Beer.query.all()
         beers_formatted = [beer.format() for beer in beers]
         return jsonify({
@@ -112,23 +114,23 @@ def create_app(testconfig=None):
                     "beers": beers_formatted,
                 })
 
-    ''' 
-        returns list of beers 
+    '''
+        returns list of beers
         city defaults to default_location in config_setings.json
     '''
     @app.route("/beers/<city>/", methods=['GET'])
     @requires_auth('get:beers')
     def get_city_beer(payload, city=default_city):
         beers_venue_list = BeerVenue.query.all()
-        beers_by_city=[]
-    
+        beers_by_city = []
+
         for beerItem in beers_venue_list:
-            venue=Venue.query.filter_by(id=beerItem.venue_id).all()
+            venue = Venue.query.filter_by(id=beerItem.venue_id).all()
             if venue[0].city.lower() == city.lower():
-                tempbeer={}
-                beer=Beer.query.filter_by(id=beerItem.beer_id).all()
+                tempbeer = {}
+                beer = Beer.query.filter_by(id=beerItem.beer_id).all()
                 # Beer class not modifiable so convert to dict and add venue info
-                tempbeer = row2dict_lambda(beer[0])
+                tempbeer = row2dict(beer[0])
                 tempbeer['venue'] = venue[0].format()
                 beers_by_city.append(tempbeer)
 
@@ -146,22 +148,22 @@ def create_app(testconfig=None):
     @app.route('/beer-details/', methods=['GET'])
     @requires_auth('get:beer-details')
     def get_beer_detail(payload):
-     
+
         body = request.get_json()
-        if body==None:
+        if body is None:
             print("NO BODY")
             abort(400)
-        try: 
-            beer = Beer.query.filter(Beer.id==body['beer_id']).one_or_none()
-            if beer == None:
-                abort(404, "Beer not found.") 
+        try:
+            beer = Beer.query.filter(Beer.id == body['beer_id']).one_or_none()
+            if beer is None:
+                abort(404, "Beer not found.")
 
             return jsonify({
                 "status_code": 200,
                 "success": True,
-                "beers": beer.format() 
+                "beers": beer.format()
             }), 200
-        except:
+        except Exception as Err:
             abort(422)
 
     '''Endpoint GET /styles/ returns list of styles
@@ -169,9 +171,9 @@ def create_app(testconfig=None):
     @app.route("/styles/")
     @requires_auth('get:styles')
     def get_styles(payload):
-        styles = Style.query.all() 
+        styles = Style.query.all()
         formatted_styles = [style.format() for style in styles]
-        
+
         return jsonify({
             "status_code": 200,
             "success": True,
@@ -179,59 +181,57 @@ def create_app(testconfig=None):
             "styles": formatted_styles
             })
 
-    '''Endpoint POST /beers 
+    '''Endpoint POST /beers
        insert new beer object into database
        Specified venue in new beer object must be valid and exist in Venue table
     '''
     @app.route("/beers/", methods=['POST'])
     @requires_auth('post:beers')
-    def create_beer(payload): 
+    def create_beer(payload):
         body = request.get_json()
-        if body==None:
+        if body is None:
             print("NO BODY")
             abort(400)
-        try: 
-
+        try:
             # check if beer exists
-            beer = Beer.query.filter(Beer.bid == body['bid'], Beer.venue_id==body['venue_id']).one_or_none()   
-            if beer!=None:
-                abort(400, "Create_beer: Beer already exists.")  
+            beer = Beer.query.filter(Beer.bid == body['bid'], Beer.venue_id == body['venue_id']).one_or_none()
+            if beer is not None:
+                abort(400, "Create_beer: Beer already exists.")
 
-            venue = Venue.query.filter(Venue.venue_id == body['venue_id']).one_or_none() 
-            if venue==None:
+            venue = Venue.query.filter(Venue.venue_id == body['venue_id']).one_or_none()
+            if venue is not None:
                 abort(400, "Create_beer: Venue not found.")
 
             new_beer = Beer(
-                bid = body['bid'],
-                name = body['name'],
-                slug = body['slug'],
-                style = body['style'],
-                brewery_name = body['brewery_name'],
-                brewery_slug = body['brewery_slug'],
-                last_seen = body['last_seen'],
-                major_style = body['major_style'],
-                rating = body['rating'],
-                user_rating = body['user_rating'],
-                abv = body['abv'],
-                url = body['url'],
-                venue_id = body['venue_id'],
-            )
-        
+                bid=body['bid'],
+                name=body['name'],
+                slug=body['slug'],
+                style=body['style'],
+                brewery_name=body['brewery_name'],
+                brewery_slug=body['brewery_slug'],
+                last_seen=body['last_seen'],
+                major_style=body['major_style'],
+                rating=body['rating'],
+                user_rating=body['user_rating'],
+                abv=body['abv'],
+                url=body['url'],
+                venue_id=body['venue_id'],
+                )
+
             new_beer.insert()
             new_beervenue = BeerVenue(
-                beer_id = new_beer.id,
-                venue_id = venue.id
-            )
+                beer_id=new_beer.id,
+                venue_id=venue.id,
+                )
             new_beervenue.insert()
-    
+
             return jsonify({
                 "status_code": 200,
                 "success": True,
                 "created": new_beer.id
             }), 200
-        except:
-            abort(422)  
-
+        except Exception as err:
+            abort(422)
 
     '''
     Endpoint PATCH /rating/
@@ -243,34 +243,33 @@ def create_app(testconfig=None):
     @app.route('/rating/', methods=['PATCH'])
     @requires_auth('patch:beer-user-rating')
     def update_beer(payload):
-        
+
         body = request.get_json()
-        if body==None:
+        if body is None:
             print("NO BODY")
             abort(400)
-        try: 
+        try:
             # check if beers exists
             beer = Beer.query.get(body['id'])
-            if beer==None:
+            if beer is None:
                 abort(404, "Update beer: beer not found.")
-            
-            updated_rating = body['user_rating']  
+
+            updated_rating = body['user_rating']
             if updated_rating:
-                beer.user_rating = updated_rating 
+                beer.user_rating = updated_rating
             beer.update()
-                
+
             return jsonify({
                 "status_code": 200,
                 "success": True,
                 "modified": beer.id
             }), 200
         except:
-            abort(422) 
-
+            abort(422)
 
     '''
     Endpoint DELETE /beers/<id>
-    require the 'delete:beers' permission        
+    require the 'delete:beers' permission
     <id> is the existing beer id
     returns deleted beer id
     '''
@@ -278,43 +277,41 @@ def create_app(testconfig=None):
     @requires_auth('delete:beers')
     def delete_beer(payload, id):
         # check if beer exists
-        beer = Beer.query.filter(Beer.id==id).one_or_none()
+        beer = Beer.query.filter(Beer.id == id).one_or_none()
 
-        if beer==None:
+        if beer is None:
             abort(404, "Delete beer: Beer not found.")
 
         venue = Venue.query.filter(Venue.venue_id == beer.venue_id).one_or_none()
-        if venue==None:
+        if venue is None:
             abort(404, "Delete beer: Venue not found.")
-        
+
         beervenue = BeerVenue.query.filter(BeerVenue.venue_id == venue.id, BeerVenue.beer_id == beer.id).one_or_none()
-        if beervenue==None:
+        if beervenue is None:
             abort(404, "Delete beer: Beer, Venue record not found.")
         print(f"beer_server::delete_beer::beervenue: { beervenue.beer_id, beervenue.venue_id}")
-        try:       
-            
+        try:
             beervenue.delete()
             beer.delete()
-            
+
             return jsonify({
                 "status_code": 200,
                 "success": True,
                 "deleted": id
             }), 200
 
-
         except:
-            abort(422)  
+            abort(422)
 
-    ''' converts rows to dictionary from list object 
+    ''' converts rows to dictionary from list object
         lambda same as def ...just concise version
     '''
-    row2dict_lambda = lambda row: {c.name: str(getattr(row, c.name)) for c in row.__table__.columns}
-    # def row2dict(row):
-    #     tempdict = {}
-    #     for column in row.__table__.columns:
-    #         tempdict[column.name] = str(getattr(row, column.name))
-    #     return tempdict
+    # row2dict_lambda = lambda row: {c.name: str(getattr(row, c.name)) for c in row.__table__.columns}
+    def row2dict(row):
+        tempdict = {}
+        for column in row.__table__.columns:
+            tempdict[column.name] = str(getattr(row, column.name))
+        return tempdict
 
     # --------------------- ERROR handlers ---------------------------------
     @app.errorhandler(404)
@@ -353,16 +350,14 @@ def create_app(testconfig=None):
             "description": error.description
         }), 405
 
-
     @app.errorhandler(500)
-    def method_not_allowed(error):
+    def internal_server_error(error):
         return jsonify({
             "success": False,
             "error": 500,
             "message": "Internal Server Error",
             "description": error.description
         }), 500
-
 
     '''
     error handler for AuthError
@@ -374,9 +369,8 @@ def create_app(testconfig=None):
             "error": ex.status_code,
             "message": ex.error['description']
         }), ex.status_code
-        
-    
-    #if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=5000, debug=True)
+
+    # if __name__  ==  "__main__":
+    # app.run(host='127.0.0.1', port=5000, debug=True)
 
     return app
