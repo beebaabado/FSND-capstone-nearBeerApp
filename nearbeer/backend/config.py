d55c52f1b22eded6f23dd313e5c1e7544e9488ca
Connie Compos <beebaabado@gmail.com>
2021-08-25 19:16:59 -0600

    Updated config.py typo postgresl to postgresql

--- nearbeer/backend/config.py
+++ nearbeer/backend/config.py
@@ -14,8 +14,8 @@ TESTING = False
 # COMPLETED IMPLEMENT DATABASE URL and set other options for SQLAlchemy
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/nearbeer'
 # DATABASE_URI='postgresql://postgres@localhost:5432/nearbeer'
-SQLALCHEMY_DATABASE_URI= 'postgresl://tymiuxxwxkccqf:b137e9dbb038c08b2b8da273150f2da4556a5ac2bcce1fe5375be98106b078ab@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d9qq2ri9p09bh1'
-DATABASE_URI='postgresl://tymiuxxwxkccqf:b137e9dbb038c08b2b8da273150f2da4556a5ac2bcce1fe5375be98106b078ab@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d9qq2ri9p09bh1'
+SQLALCHEMY_DATABASE_URI= 'postgresql://tymiuxxwxkccqf:b137e9dbb038c08b2b8da273150f2da4556a5ac2bcce1fe5375be98106b078ab@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d9qq2ri9p09bh1'
+DATABASE_URI='postgresql://tymiuxxwxkccqf:b137e9dbb038c08b2b8da273150f2da4556a5ac2bcce1fe5375be98106b078ab@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d9qq2ri9p09bh1'
 
 # Prevent warning concerning use of Flask-SQLAlchemy event system causing overhead in memory usage
 SQLALCHEMY_TRACK_MODIFICATIONS = False
