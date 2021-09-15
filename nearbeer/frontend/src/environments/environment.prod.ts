9b6cc18f59187e704e8a62c561b886333cf77b86
Connie Compos <beebaabado@gmail.com>
2021-09-15 00:17:30 -0600

    add env variables to prod env

--- nearbeer/frontend/src/environments/environment.prod.ts
+++ nearbeer/frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  //apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url  run heroku locally  with 'heroku local web'
+  apiServerUrl: 'https://capstone-nearbeer-app.herokuapp.com',
+  auth0: {
+    url: 'product-demos.us', // the auth0 domain prefix
+    audience: 'beernear', // the audience set for the auth0 app
+    clientId: 'ziR3Cuw3SWmFhkPWfThF9LDz7gkXnMH6', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
 };
